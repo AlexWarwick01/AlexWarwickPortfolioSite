c4be952304b0c0499b7a0b867db363da52fe2871
Alex Warwick <alexwarwick01@gmail.com>
2025-02-04 19:06:22 +0000

    Adds automatic updating of Java Experience, Adds more info

--- js/age.js
+++ js/age.js
@@ -1,6 +1,13 @@
 var ageEl = document.getElementById("age");
+var javaExpEl = document.getElementById("javaExperience")
 
 setInterval(() => {
+	// For Age
 	let time = (new Date() - new Date(960217232000)) / (1000 * 60 * 60 * 24 * 365.25); // milliseconds per year
-	ageEl.innerText = time.toString().substring(0, 6);
+	ageEl.innerText = time.toString().substring(0, 2);
+
+
+	// For Experience in Java
+	let javaExpTime = (new Date() - new Date(1562000279000)) / (1000 * 60 * 60 * 24 * 365.25); // milliseconds per year
+	javaExpEl.innerText = javaExpTime.toString().substring(0, 4);
 }, 50);
